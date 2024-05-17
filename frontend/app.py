0f6ea7a45fac2bdc610dbaf977ba25823411bd74
Dongjoo <ddtss95@gmail.com>
2024-05-17 20:00:08 +0900

    initial commit

---
+++ frontend/app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+# 페이지 속성
+st.set_page_config(
+    page_title="Aroma Alchemist - AA",
+    page_icon="🧴",
+)
+
+st.title("Aroma Alchemist")
+st.markdown(
+    """
+Welcome!
+
+I'm your personal perfume advisor.
+
+You can tell me what type of perfume you're looking for, or simply select your preferences on the sidebar for a quick recommendation!
+"""
+)
+
