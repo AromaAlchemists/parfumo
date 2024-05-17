162ae9b98d17aa1013239b5b831e033b8e698683
Dongjoo Kim <ddtss95@gmail.com>
2024-05-17 23:47:37 +0900

    feat: sidebar UI 추가 및 조건 선택 기능 추가

--- frontend/app.py
+++ frontend/app.py
@@ -1,15 +1,33 @@
 import streamlit as st
 
-# 페이지 속성
+
+# values that will be sent to the server
+val_audience = [0, 0, 0, 0]
+val_season = [0, 0, 0, 0]
+val_occasion = [0, 0, 0, 0, 0, 0]
+val_text = ""
+
+# option lists for each category
+opts_audience = ["Youthful", "Mature", "Feminine", "Masculine"]
+opts_season = ["Spring", "Summer", "Fall", "Winter"]
+opts_occasion = ["Leisure", "Daily", "Night out", "Business", "Sport", "Evening"]
+optcnt_audience = len(opts_audience)
+optcnt_season = len(opts_season)
+optcnt_occasion = len(opts_occasion)
+
+
+# page configuration (title on the tab)
 st.set_page_config(
     page_title="Aroma Alchemist - AA",
     page_icon="🧴",
 )
 
+
+# UI - intro
 st.title("Aroma Alchemist")
 st.markdown(
     """
-Welcome!
+#### Welcome!
 
 I'm your personal perfume advisor.
 
@@ -17,3 +35,69 @@ You can tell me what type of perfume you're looking for, or simply select your p
 """
 )
 
+
+# UI - sidebar / Quick Recommendation
+with st.sidebar:
+    st.header("Quick Recommendation", divider="rainbow")
+
+    # toggle button for page switching (simple(default) <-> detailed)
+    isdetailed = st.toggle("Fine-tune Options")
+
+    # UI for simple choice
+    if not isdetailed:
+        st.subheader("Audience:")
+        for i in range(optcnt_audience):
+            val_audience[i] = st.checkbox(opts_audience[i])
+        "\n"
+        st.subheader("Season:")
+        for i in range(optcnt_season):
+            val_season[i] = st.checkbox(opts_season[i])
+        "\n"
+        st.subheader("Occasion:")
+        for i in range(optcnt_occasion):
+            val_occasion[i] = st.checkbox(opts_occasion[i])
+        "\n"
+
+    # UI for detailed choice
+    else:
+        st.subheader("Audience:")
+        for i in range(optcnt_audience):
+            val_audience[i] = st.slider(
+                opts_audience[i],
+                max_value=100,
+                step=10,
+                key=("slider_" + opts_audience[i]),
+            )
+        "\n"
+        st.subheader("Season:")
+        for i in range(optcnt_season):
+            val_season[i] = st.slider(
+                opts_season[i],
+                max_value=100,
+                step=10,
+                key=("slider_" + opts_season[i]),
+            )
+        "\n"
+        st.subheader("Occasion:")
+        for i in range(optcnt_occasion):
+            val_occasion[i] = st.slider(
+                opts_occasion[i],
+                max_value=100,
+                step=10,
+                key=("slider_" + opts_occasion[i]),
+            )
+        "\n"
+
+    # text input area for additional input
+    with st.expander("Additional Input:"):
+        val_text = st.text_area(
+            label="text_area",
+            placeholder="Type for more personal preferences",
+            label_visibility="collapsed",
+        )
+
+
+val_audience
+val_season
+val_occasion
+val_text
